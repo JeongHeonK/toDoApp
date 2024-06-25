a7c162b8d743c861a7472414bbc4439f61f48f1e
김정헌 <metronom7703@naver.com>
2024-06-25 13:10:24 +0900

    ✨ Header component 생성
    header component 생성

---
+++ app/components/header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "/public/img/Logo.png";
+
+export default function Header() {
+  return (
+    <header className="h-16 p-0 border-b border-b-slate-200">
+      <ul className="list-non py-2  max-w-screen-lg mx-auto">
+        <li>
+          <Link href="/">
+            <Image src={logoImg} alt="logo"></Image>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Header from "./components/header";
+
 export default function Home() {
   return (
     <main>
+      <Header />
       <h1>To-Do</h1>
     </main>
   );
