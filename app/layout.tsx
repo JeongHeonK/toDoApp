ae17726189e7be02314d66d4c6b922bd4a303c66
김정헌 <metronom7703@naver.com>
2024-06-25 12:31:09 +0900

    💄 Set default font
    set default font as NanumSquare

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'HOME',
-  description: 'add your work-to-do',
+  title: "HOME",
+  description: "add your work-to-do",
 };
 
 export default function RootLayout({
@@ -16,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
