0ba21037c8a3e8fe1bfc0a2eff9f4f8e23a2cdee
김정헌 <metronom7703@naver.com>
2024-06-25 22:38:33 +0900

    ♻️ 서버 코드 업데이트
    swith 문으로 변경, 수정 테스트 완료

--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -1,10 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function todoHandler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    console.log("서버테스트");
-    res.status(200).json("응답 테스트");
-  } else {
-    res.status(404).json("fail test");
+  switch (req.method) {
+    case "GET":
+      console.log("서버테스트");
+      res.status(200).json("응답 테스트");
+      break;
+    case "PATCH":
+      console.log("수정 요청 테스트");
+      res.status(200).json("수정 요청 응답");
+      break;
+    default:
+      res.status(404).json("fail test");
+      break;
   }
 }
