0d38250592996bb6e72314076af555d793fa743b
김정헌 <metronom7703@naver.com>
2024-06-26 15:57:23 +0900

    ♻️ 클라이언트 컴포넌트로 수정
    클라이언트 컴포넌트로 수정, 제어 컴포넌트로 변환

--- app/components/MemoInput.tsx
+++ app/components/MemoInput.tsx
@@ -1,4 +1,15 @@
-export default function MemoInput() {
+import { ChangeEvent } from "react";
+
+type Props = {
+  defaultValue: string | undefined;
+  onChange: (name: string, value: string) => void;
+};
+
+export default function MemoInput({ onChange, defaultValue }: Props) {
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    onChange(name, value);
+  };
   return (
     <div className="grow relative rounded-xl bg-memo-pattern flex flex-col justify-center">
       <p className="absolute w-fit left-0 right-0 mx-auto top-3 text-xl text-amber-800">
@@ -6,7 +17,9 @@ export default function MemoInput() {
       </p>
       <textarea
         rows={1}
-        name="toDoMemo"
+        onChange={handleChange}
+        name="memo"
+        value={defaultValue}
         placeholder="여기에 입력해주세요."
         className="w-full h-auto rounded-xl resize-none text-center bg-transparent focus:outline-none"
       />
--- app/items/[itemId]/page.tsx
+++ app/items/[itemId]/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import mockupData from "../../mockup.json";
+import { FormEvent, useState } from "react";
 
-import Button from "@/app/components/Button";
 import DetailTitle from "@/app/components/DetailTitle";
 import ImageInput from "@/app/components/ImageInput";
 import MemoInput from "@/app/components/MemoInput";
+import { ChangeEvent } from "react";
+import { json } from "stream/consumers";
 
 export default function ItemDetail({
   params: { itemId },
@@ -11,23 +14,71 @@ export default function ItemDetail({
   params: { itemId: string };
 }) {
   const targetData = mockupData.find((item) => item.id === Number(itemId));
+  const [detailData, setDetailData] = useState({
+    name: targetData?.name,
+    memo: "",
+    imageUrl: "",
+    isCompleted: targetData?.isCompleted,
+  });
+
+  const handleChange = (name: string, value: string) => {
+    setDetailData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitClick = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await fetch("/api/todo", {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(detailData),
+    });
+    const result = await response.json();
+
+    console.log(result);
+  };
+
+  const handleDeleteClick = async () => {
+    const response = await fetch("/api/todo", {
+      method: "DELETE",
+      body: JSON.stringify(itemId),
+    });
+    const result = await response.json();
+
+    console.log(result);
+  };
+
   return (
-    <form className="max-w-screen-lg mx-auto mt-6">
+    <form onSubmit={handleSubmitClick} className="max-w-screen-lg mx-auto mt-6">
       <DetailTitle
+        value={detailData.name}
+        onChange={handleChange}
         isCompleted={targetData?.isCompleted}
         title={targetData?.name}
       />
       <div className="flex gap-3 mt-6">
-        <ImageInput />
-        <MemoInput />
+        <ImageInput
+          name="imageUrl"
+          value={detailData.imageUrl}
+          onChange={handleChange}
+        />
+        <MemoInput defaultValue={detailData.memo} onChange={handleChange} />
       </div>
       <div className="flex justify-end gap-3 mt-6">
-        <Button buttonType="submit" color="bg-slate-200">
+        <button className="w-40 shadow-shadowCustom rounded-full border-2 border-slate-900 h-14 bg-slate-200">
           ✓ 수정 완료
-        </Button>
-        <Button buttonType="button" color="bg-rose-500 text-white">
+        </button>
+        <button
+          type="button"
+          onClick={handleDeleteClick}
+          className="w-40 shadow-shadowCustom rounded-full border-2 border-slate-900 h-14 bg-rose-500 text-white"
+        >
           X 삭제하기
-        </Button>
+        </button>
       </div>
     </form>
   );
