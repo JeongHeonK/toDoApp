c97c8e32541d6aefd253c691762e7abb8c53ffea
김정헌 <metronom7703@naver.com>
2024-06-26 15:58:02 +0900

    ♻️ 수정 삭제 기능 추가
    수정 삭제 기능 추가

--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -7,8 +7,12 @@ export default function todoHandler(req: NextApiRequest, res: NextApiResponse) {
       res.status(200).json("응답 테스트");
       break;
     case "PATCH":
-      console.log("수정 요청 테스트");
+      console.log(req.body);
       res.status(200).json("수정 요청 응답");
+      break;
+    case "DELETE":
+      console.log(req.body);
+      res.status(200).json("삭제 요청 완료");
       break;
     default:
       res.status(404).json("fail test");
