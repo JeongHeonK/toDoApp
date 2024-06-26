1cbb5259efff2742ba40ff552d3b3f80d580d68d
김정헌 <metronom7703@naver.com>
2024-06-26 10:39:31 +0900

    💄 디테일 페이지 스타일 구현
    디테일 페이지 스타일 구현

--- app/items/[itemId]/page.tsx
+++ app/items/[itemId]/page.tsx
@@ -1,4 +1,7 @@
 import mockupData from "../../mockup.json";
+import Image from "next/image";
+import StateIndicator from "@/app/components/StateIndicator";
+import uploadImg from "/public/img/uploadImg.png";
 
 export default function ItemDetail({
   params: { itemId },
@@ -7,10 +10,54 @@ export default function ItemDetail({
 }) {
   const targetData = mockupData.find((item) => item.id === Number(itemId));
   return (
-    <div>
-      <p>{targetData?.id}</p>
-      <p>{targetData?.name}</p>
-      <p>{targetData?.isCompleted}</p>
-    </div>
+    <form className="max-w-screen-lg mx-auto mt-6">
+      <div className="border-2 border-slate-900 rounded-xl py-2 flex justify-center gap-4 bg-white">
+        <StateIndicator isCompleted={targetData?.isCompleted} />
+        <input
+          className="underline w-fit focus:outline-none"
+          type="text"
+          defaultValue={targetData?.name}
+        />
+      </div>
+      <div className="flex gap-3 mt-6">
+        <label
+          htmlFor="imageUpload"
+          className="relative w-2/5 h-80 rounded-xl border-2 border-dashed cursor-pointer"
+        >
+          <Image
+            className="absolute right-0 left-0 mx-auto top-0 bottom-0 my-auto"
+            src={uploadImg}
+            alt="upload-logo"
+            width={54}
+          />
+          <div className="size-16 text-slate-500 bg-slate-200 text-4xl rounded-full text-center pt-3 absolute bottom-2 right-2">
+            +
+          </div>
+          <input type="file" id="imageUpload" hidden />
+        </label>
+        <div className="grow relative rounded-xl bg-memo-pattern flex flex-col justify-center">
+          <p className="absolute w-fit left-0 right-0 mx-auto top-3 text-xl text-amber-800">
+            Memo
+          </p>
+          <textarea
+            rows={1}
+            name="toDoMemo"
+            placeholder="여기에 입력해주세요."
+            className="w-full h-auto rounded-xl resize-none text-center bg-transparent focus:outline-none"
+          />
+        </div>
+      </div>
+      <div className="flex justify-end gap-3 mt-6">
+        <button className="w-40 bg-slate-200 shadow-shadowCustom rounded-full border-2 border-slate-900 h-14">
+          ✓ 수정 완료
+        </button>
+        <button
+          type="button"
+          className="w-40 bg-rose-500 shadow-shadowCustom rounded-full border-2 border-slate-900 h-14 text-white"
+        >
+          X 삭제하기
+        </button>
+      </div>
+    </form>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "memo-pattern": "url(/img/memo.png)",
       },
       boxShadow: {
         shadowCustom: "4.5px 3px 0px 2px #0F172A",
