086744ee5b8122a5929b013de352fa63d4e938f2
김정헌 <metronom7703@naver.com>
2024-06-27 22:50:47 +0900

    ♻️ 수정 및 삭제 api 연결 완료
    수정 및 삭제 api 연결 완료

--- app/components/MemoInput.tsx
+++ app/components/MemoInput.tsx
@@ -10,18 +10,18 @@ export default function MemoInput({ onChange, defaultValue }: Props) {
     const { name, value } = e.target;
     onChange(name, value);
   };
+
   return (
     <div className="grow relative rounded-xl bg-memo-pattern flex flex-col justify-center">
       <p className="absolute w-fit left-0 right-0 mx-auto top-3 text-xl text-amber-800">
         Memo
       </p>
       <textarea
-        rows={1}
         onChange={handleChange}
         name="memo"
         value={defaultValue}
         placeholder="여기에 입력해주세요."
-        className="w-full h-auto rounded-xl resize-none text-center bg-transparent focus:outline-none"
+        className="w-full h-auto rounded-xl resize-none text-center bg-transparent focus:outline-none overflow:hidden"
       />
     </div>
   );
