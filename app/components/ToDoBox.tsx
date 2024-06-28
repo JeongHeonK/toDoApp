fd366a7beb002bd0b90e4879e0b189bafd65ed36
김정헌 <metronom7703@naver.com>
2024-06-28 12:21:35 +0900

    💄 할일 기본 이미지 추가
    할일 기본 이미지 추가

--- app/components/ToDoBox.tsx
+++ app/components/ToDoBox.tsx
@@ -13,17 +13,25 @@ type Props = {
 export default function ToDoBox({ isCompleted, works, setWorkData }: Props) {
   const worksArray = works?.filter((item) => item.isCompleted === isCompleted);
   return (
-    <article className="w-11/12 lg:w-1/2 h-54 lg:h-60 mt-10 overflow-scroll ">
+    <article className="w-11/12 lg:w-1/2 h-64 lg:h-60 mt-10 overflow-scroll">
       <ToDoTitle isCompleted={isCompleted} />
-      {worksArray?.map((item: Data) => (
-        <ToDoItem
-          isCompleted={item.isCompleted}
-          id={item.id}
-          name={item.name}
-          key={item.id}
-          setWorkData={setWorkData}
-        />
-      ))}
+      {worksArray && worksArray?.length > 0 ? (
+        worksArray?.map((item: Data) => (
+          <ToDoItem
+            isCompleted={item.isCompleted}
+            id={item.id}
+            name={item.name}
+            key={item.id}
+            setWorkData={setWorkData}
+          />
+        ))
+      ) : (
+        <div
+          className={`w-full h-40 mt-10 bg-no-repeat bg-center bg-contain mt-0 ${
+            isCompleted ? "bg-doneDefault-sm" : "bg-todoDefault-sm"
+          } lg:${isCompleted ? "bg-doneDefault" : "bg-todoDefault"}`}
+        ></div>
+      )}
     </article>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,10 @@ const config: Config = {
         "logo-sm": "url(/img/logo-sm.png)",
         "btn-edit": "url(/img/btn-edit.png)",
         "btn-plus": "url(/img/btn-plus.png)",
+        doneDefault: "url(/img/doneDefault.png)",
+        "doneDefault-sm": "url(/img/doneDefault-sm.png)",
+        todoDefault: "url(/img/todoDefault.png)",
+        "todoDefault-sm": "url(/img/todoDefault-sm.png)",
       },
       boxShadow: {
         shadowCustom: "4.5px 3px 0px 2px #0F172A",
